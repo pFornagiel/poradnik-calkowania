9361f39426507d6e57573d87aa7c3e391a58e21b
Massny <pawel.fornagiel2@gmail.com>
2024-12-04 23:32:06 +0100

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/my-project",
+  base: "/poradnik-calkowania",
   plugins: [tsconfigPaths(), react()],
 });
